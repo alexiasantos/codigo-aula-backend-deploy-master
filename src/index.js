85a888f2ee5ca128939ce3f1aa2c446bc0dc15a2
Alexia Santos <alexiaosantos17@gmail.com>
2023-11-29 21:00:05 -0300

    segunda alteracao

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 require('dotenv').config()
 const express = require('express')
-const knex = require('knex')
-const app = express()
+const knex = require('./conexao');
+const app = express();
 
 app.use(express.json())
 
 app.get('/', async (req, res) => {
-	const carros = knex('carros')
-	return res.json(carros)
+	try {
+		const carros = await knex('carros');
+		return res.json(carros)
+	} catch (error) {
+		return res.status(500).json({ mensagem: 'Erro do servidor' })
+	}
+
 })
 
 const port = process.env.PORT || 3000
